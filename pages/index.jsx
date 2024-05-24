31576447c8a3e3b7e7b789701dce71e061978d64
Mykyta Makhorin <nikitos2159@gmail.com>
2024-05-24 02:05:09 +0200

    update gsap animation

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
 "use client";
-import React, { useEffect } from 'react'
-// import '@styles/globals.css';
+import React, { useEffect } from 'react';
 import CustomCursor from '@components/Cursors';
 import NavbarMenu from '@components/NavbarMenu';
 import Main from "@components/Main";
-import About from "@components/About"
+import About from "@components/About";
 import Skills from '@components/Skills';
 import Projects from '@components/Projects';
 import { Reveal } from '@components/Reveal';
@@ -15,48 +14,59 @@ import { PanelScroll } from '@components/PanelScroll';
 
 const Home = ({ children }) => {
     useEffect(() => {
+        const setHeight = () => {
+            const height = window.innerHeight;
+            document.documentElement.style.height = `${height}px`;
+            document.body.style.height = `${height}px`;
+            const wrapper = document.getElementById('wrapper');
+            if (wrapper) {
+                wrapper.style.height = `${height}px`;
+            }
+        };
+
+        // Initial set height
+        setHeight();
+
+        // Update height on resize
+        window.addEventListener('resize', setHeight);
+
+        // Cleanup event listener on unmount
+        return () => {
+            window.removeEventListener('resize', setHeight);
+        };
+    }, []);
+
+    useEffect(() => {
         PanelScroll();
-    }, [])
+    }, []);
+
     return (
-        // <Template>
-
-        <div>
-
+        <div id="wrapper">
             <div className="hidden md:flex">
                 {/* <CustomCursor /> */}
             </div>
             <NavbarMenu />
-
             <div className="circle"></div>
             <div className="main">
                 <div className="gradient" />
             </div>
             <main className='app'>
-
                 <div className="panel blue">
                     <Main />
                 </div>
-
                 <Reveal>
                     <div className="panel fiol">
                         <Select />
                     </div>
                 </Reveal>
-
                 <Reveal>
                     <div className="panel red">
                         <Slider />
                     </div>
                 </Reveal>
-
                 {children}
             </main>
-
-
         </div>
-        // </Template>
-
-
     );
 }
 
