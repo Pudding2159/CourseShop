e54c922f47a9700068f30a5350c842e4f3b29f0f
Mykyta Makhorin <nikitos2159@gmail.com>
2024-06-03 19:10:50 +0200

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,10 +1,9 @@
 "use client";
-import React, { useEffect } from 'react'
-import '@styles/globals.css';
+import React, { useEffect } from 'react';
 import CustomCursor from '@components/Cursors';
 import NavbarMenu from '@components/NavbarMenu';
 import Main from "@components/Main";
-import About from "@components/About"
+import About from "@components/About";
 import Skills from '@components/Skills';
 import Projects from '@components/Projects';
 import { Reveal } from '@components/Reveal';
@@ -12,51 +11,41 @@ import Select from '@components/Select';
 import Slider from '@components/Slider';
 import Template from '@components/template';
 import { PanelScroll } from '@components/PanelScroll';
-import '@styles/TextHax.css'
+import '@styles/globals.css';
+
 const Home = ({ children }) => {
+    
     useEffect(() => {
         PanelScroll();
-    }, [])
+    }, []);
+
     return (
-        // <Template>
-
-        <div>
-
+        <div id="wrapper">
             <div className="hidden md:flex">
                 {/* <CustomCursor /> */}
             </div>
             <NavbarMenu />
-
             <div className="circle"></div>
             <div className="main">
                 <div className="gradient" />
             </div>
             <main className='app'>
-
-                <div className="panel blue ">
+                <div className="panel blue">
                     <Main />
                 </div>
-
                 {/* <Reveal> */}
                     <div className="panel fiol">
                         <Select />
                     </div>
                 {/* </Reveal> */}
-
-                {/* <Reveal> */}
+                <Reveal>
                     <div className="panel red">
-                        <Skills />
+                        <Slider />
                     </div>
-                {/* </Reveal> */}
-
+                </Reveal>
                 {children}
             </main>
-
-
         </div>
-        // </Template>
-
-
     );
 }
 
